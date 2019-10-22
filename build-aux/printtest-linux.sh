d770f5ca984c4d4a14bb04d7f7b37dfd8118d87f
Marco Confalonieri <marco.confalonieri@elits.com>
2019-10-22 23:19:22 +0200

    Corrected linux test script

--- build-aux/printtest-linux.sh
+++ build-aux/printtest-linux.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for logfile in `ls -1 test_*.log`; do tap2junit $file $file.xml; cat $file.xml; done
+for logfile in `ls -1 test_*.log`; do tap2junit $logfile $logfile.xml; cat $logfile.xml; done
